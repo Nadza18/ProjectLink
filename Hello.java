da754251029640f0d6bfd074d7a4cd39d17549cb
Nadza18 <70228064+Nadza18@users.noreply.github.com>
2021-08-25 20:08:14 -0400

    Java_first_class

---
+++ Hello.java
@@ -0,0 +1,19 @@
+
+/*********************************************
+* Java Program Name
+**********************************************
+* Student Name (Nadeem Abdulmelik)
+* Date Created August 25, 2021
+* Course Number and Section Number 
+**********************************************/
+
+public class Hello {
+	public static void main(String[] args){
+	System.out.println("<Your Name>");
+	System.out.println("<Jave Program Name>");
+	System.out.println("<Program/Project Description>");
+	System.out.println("<Date Created>");
+	System.out.println("<Course Number and Section Number>");
+	System.out.println();
+	}
+}
